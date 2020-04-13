e14f3aa0df514091ece125c0336f46149c9d1a56
thaerrm <33045802+thaerrm@users.noreply.github.com>
2020-04-12 23:54:48 -0500

    2nd test unsuccessful

--- src/Tests/PersonTest.java
+++ src/Tests/PersonTest.java
@@ -1,8 +1,11 @@
 package Tests;
 import Interfaces.Observer;
+import Interfaces.Subject;
 import org.junit.Assert;
 import org.junit.Test;
 import Subject.Person;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class PersonTest {
@@ -13,6 +16,13 @@ public class PersonTest {
         Person person = new Person();
         person.setPersonName("Name");
         String personName = person.getPersonName();
-        Assert.assertEquals(personName,"Name");
+        Assert.AssertEquals(personName,"Name");
+    }
+    public void subjectTest()
+    {
+        Person person = new Person();
+        person.setSubject("");
+        String subject = person.getSubject();
+        Assert.assertEquals(subject, (""));
     }
 }
